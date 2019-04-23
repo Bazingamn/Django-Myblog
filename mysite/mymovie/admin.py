9bd6c932cd8070c9cbc3bf0cbe46a789bc5bfd8b
Bruce Wang <1804319795@qq.com>
2019-04-23 13:58:37 +0800

    add a question in polls

--- mysite/mymovie/admin.py
+++ mysite/mymovie/admin.py
@@ -4,3 +4,5 @@ from . import models
 
 # Register your models here.
 admin.site.register(models.Movies)
+admin.site.register(models.Weathers)
+admin.site.register(models.JDPhone)
--- mysite/mymovie/models.py
+++ mysite/mymovie/models.py
@@ -24,7 +24,7 @@ class Weathers(models.Model):
     strength = models.CharField(max_length=255)
 
     def __str__(self):
-        return self.weather
+        return self.date
 
 
 class JDPhone(models.Model):
